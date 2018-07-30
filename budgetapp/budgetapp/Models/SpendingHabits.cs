d2ca3720d144d589d2f48a632cf11f9812f4200d
Jacob <jakexmueller@gmail.com>
2018-07-30 08:17:04 -0500

    SpendingHabits model

---
+++ budgetapp/budgetapp/Models/SpendingHabits.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace budgetapp.Models
+{
+    public class SpendingHabits
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("ApplicationUser")]
+        public string UserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public DateTime Date { get; set; }
+        public int CashTotal { get; set; }
+        public double AssetTotal { get; set; }
+        public bool BudgetIndicator { get; set; }
+    }
+}
