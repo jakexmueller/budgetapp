1263241c1b6c762b1653bddd01d6d6e72a98f4d4
Jacob <jakexmueller@gmail.com>
2018-07-30 08:19:38 -0500

    SpendingHabits controller and views

---
+++ budgetapp/budgetapp/Controllers/SpendingHabitsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using budgetapp.Models;
+
+namespace budgetapp.Controllers
+{
+    public class SpendingHabitsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: SpendingHabits
+        public ActionResult Index()
+        {
+            var spendingHabits = db.SpendingHabits.Include(s => s.ApplicationUser);
+            return View(spendingHabits.ToList());
+        }
+
+        // GET: SpendingHabits/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SpendingHabits spendingHabits = db.SpendingHabits.Find(id);
+            if (spendingHabits == null)
+            {
+                return HttpNotFound();
+            }
+            return View(spendingHabits);
+        }
+
+        // GET: SpendingHabits/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.ApplicationUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: SpendingHabits/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,UserId,Date,CashTotal,AssetTotal,BudgetIndicator")] SpendingHabits spendingHabits)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SpendingHabits.Add(spendingHabits);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserId = new SelectList(db.ApplicationUsers, "Id", "Email", spendingHabits.UserId);
+            return View(spendingHabits);
+        }
+
+        // GET: SpendingHabits/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SpendingHabits spendingHabits = db.SpendingHabits.Find(id);
+            if (spendingHabits == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.ApplicationUsers, "Id", "Email", spendingHabits.UserId);
+            return View(spendingHabits);
+        }
+
+        // POST: SpendingHabits/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,UserId,Date,CashTotal,AssetTotal,BudgetIndicator")] SpendingHabits spendingHabits)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(spendingHabits).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.ApplicationUsers, "Id", "Email", spendingHabits.UserId);
+            return View(spendingHabits);
+        }
+
+        // GET: SpendingHabits/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SpendingHabits spendingHabits = db.SpendingHabits.Find(id);
+            if (spendingHabits == null)
+            {
+                return HttpNotFound();
+            }
+            return View(spendingHabits);
+        }
+
+        // POST: SpendingHabits/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SpendingHabits spendingHabits = db.SpendingHabits.Find(id);
+            db.SpendingHabits.Remove(spendingHabits);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ budgetapp/budgetapp/Migrations/201807301318124_SpendingHabitsModel.cs
@@ -0,0 +1,16 @@
+namespace budgetapp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SpendingHabitsModel : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
